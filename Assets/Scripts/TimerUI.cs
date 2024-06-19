a9fb21294416f3d7b3f8612f8808073180559452
generasirabbani <generasirabbani@apps.ipb.ac.id>
2024-06-19 11:16:35 +0700

    added timer

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TimerUI : MonoBehaviour
+{
+    public TMP_Text timerText;
+    float startTime = 60;
+    float min = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        min += Time.fixedDeltaTime;
+        timerText.text = Mathf.FloorToInt(startTime - min).ToString();
+    }
+}
