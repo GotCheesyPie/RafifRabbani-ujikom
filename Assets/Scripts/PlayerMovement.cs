77531454214cb63d2e56c40862bb9f8614aeee43
generasirabbani <generasirabbani@apps.ipb.ac.id>
2024-06-19 10:05:48 +0700

    added start menu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    public float playerSpeed;
+    public Animator animator;
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        animator.SetFloat("rightSpeed", move.x);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
